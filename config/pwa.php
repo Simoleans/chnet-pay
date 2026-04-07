d759a4158c3728d7d53d270a635069fcb43a4662
Fran <francisco.hernandez@linktic.com>
2026-04-06 21:56:36 -0400

    pwa activo

--- config/pwa.php
+++ config/pwa.php
@@ -28,8 +28,15 @@ return [
         'icons' => [
             [
                 'src' => 'img/logo.png',
+                'sizes' => '192x192',
+                'type' => 'image/png',
+                'purpose' => 'any maskable',
+            ],
+            [
+                'src' => 'img/logo.png',
                 'sizes' => '512x512',
                 'type' => 'image/png',
+                'purpose' => 'any maskable',
             ],
         ],
     ],
